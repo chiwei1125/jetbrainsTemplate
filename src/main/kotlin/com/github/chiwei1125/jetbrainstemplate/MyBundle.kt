6095772b94db6e0035445af2a470bcf3efdfe13f
GitHub Action <action@github.com>
2023-01-18 03:35:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/chiwei1125/jetbrainstemplate/MyBundle.kt
+++ src/main/kotlin/com/github/chiwei1125/jetbrainstemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.chiwei1125.jetbrainstemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/chiwei1125/jetbrainstemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/chiwei1125/jetbrainstemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.chiwei1125.jetbrainstemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chiwei1125.jetbrainstemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
